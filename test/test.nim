94f9a67f98a49e779bef7cee66ccb3080233e4ce
bung87 <crc32@qq.com>
2020-08-12 15:59:52 +0800

    use nimble test

--- test/test.nim
+++
@@ -1,7 +0,0 @@
-#    AsyncMysql - Asynchronous MySQL connector written in pure Nim
-#        (c) Copyright 2017 Wang Tong
-#
-#    See the file "LICENSE", included in this distribution, for
-#    details about the copyright.
-
-import test_connection, test_pool
